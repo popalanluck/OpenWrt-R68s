b1f6d118ebc5118715218aaae7fc297fe4ce1597
popalanluck <81753503+popalanluck@users.noreply.github.com>
2023-05-06 18:48:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,6 @@
 # =================================================================
 
 # 添加软件源
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 
