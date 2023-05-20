325d8fadc17b62860a7940130bf63bd0cec036ff
popalanluck <81753503+popalanluck@users.noreply.github.com>
2023-05-20 15:30:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,8 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 # ===============================================================
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.6.200/g' package/base-files/files/bin/config_generate
 
 # 移除重复软件包
 rm -rf feeds/luci/themes/luci-theme-argon
