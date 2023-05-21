e3404e9ead56ac3b132756971c9d973114fae30b
popalanluck <81753503+popalanluck@users.noreply.github.com>
2023-05-21 17:05:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,18 +9,46 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 # ===============================================================
+
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.6.200/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate   # 定制默认IP
+
+# Configure pppoe connection
+#uci set network.wan.proto=pppoe
+#uci set network.wan.username='yougotthisfromyour@isp.su'
+#uci set network.wan.password='yourpassword'
 
 # 移除重复软件包
-rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf package/luci-theme-argon
+rm -rf package/feeds/kenzo/alist
+rm -rf package/feeds/kenzo/luci-app-alist
+# rm -rf package/network/services/fullconenat
+# git clone -b main --single-branch https://github.com/lxz1104/openwrt-fullconenat package/network/services/fullconenat
+
+# rm -rf package/feeds/kenzo/luci-theme-argone
+# rm -rf package/feeds/kiddin9/luci-base
+# rm -rf package/feeds/kiddin9/luci-theme-argon
+# rm -rf package/feeds/kiddin9/firewall
+# rm -rf package/feeds/kiddin9/firewall4
+# rm -rf package/feeds/kiddin9/luci-app-apinger
+# rm -rf package/feeds/kiddin9/luci-app-keepalived
+# rm -rf package/feeds/kiddin9/luci-app-lorawan-basicstation
 
 # Themes
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/feeds/kiddin9/luci-theme-argon
 
 # 取消bootstrap为默认主题，将默认主题改为 argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
+
+# 添加额外软件包
+
+
+# 修改配置
+sed -i "s|'enabled'|enabled|g" package/feeds/nas_luci/luci-app-istorex/root/etc/config/istorex
+sed -i "s|'model'|model|g" package/feeds/nas_luci/luci-app-istorex/root/etc/config/istorex
+sed -i "s|'wizard'|'router'|g" package/feeds/nas_luci/luci-app-istorex/root/etc/config/istorex
 
 # Disable bridge firewalling by default
 echo '# Disable bridge firewalling by default' >>package/base-files/files/etc/sysctl.conf
@@ -29,10 +57,19 @@ echo 'net.bridge.bridge-nf-call-ip6tables=0' >>package/base-files/files/etc/sysc
 echo 'net.bridge.bridge-nf-call-iptables=0' >>package/base-files/files/etc/sysctl.conf
 
 # Disable bridge firewalling for docker
+sed -i 's/enable/Disable/g' feeds/packages/utils/dockerd/files/etc/sysctl.d/sysctl-br-netfilter-ip.conf
 sed -i 's/ip6tables=1/ip6tables=0/g' feeds/packages/utils/dockerd/files/etc/sysctl.d/sysctl-br-netfilter-ip.conf
 sed -i 's/iptables=1/iptables=0/g' feeds/packages/utils/dockerd/files/etc/sysctl.d/sysctl-br-netfilter-ip.conf
 
+# sed -i "s|'enabled'|enabled|g" package/feeds/kiddin9/luci-app-istorex/root/etc/config/istorex
+# sed -i "s|'model'|model|g" package/feeds/kiddin9/luci-app-istorex/root/etc/config/istorex
+# sed -i "s|'wizard'|'router'|g" package/feeds/kiddin9/luci-app-istorex/root/etc/config/istorex
+
+# 科学上网插件
+
+
+# 科学上网插件依赖
 
 # 删除编译出错的无用补丁
-
+# rm -rf target/linux/rockchip/patches-6.1/113-ethernet-stmicro-stmmac-Add-SGMII-QSGMII-support.patch
 # rm -rf target/linux/generic/backport-5.15/430-v6.3-ubi-Fix-failure-attaching-when-vid_hdr-offset-equals.patch
