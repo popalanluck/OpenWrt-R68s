f6f83be6fa16e7fe5a13284f06c5e10b471e0b2d
Alan <81753503+popalanluck@users.noreply.github.com>
2023-06-02 20:17:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -73,3 +73,8 @@ sed -i 's/iptables=1/iptables=0/g' feeds/packages/utils/dockerd/files/etc/sysctl
 # 删除编译出错的无用补丁
 # rm -rf target/linux/rockchip/patches-6.1/113-ethernet-stmicro-stmmac-Add-SGMII-QSGMII-support.patch
 # rm -rf target/linux/generic/backport-5.15/430-v6.3-ubi-Fix-failure-attaching-when-vid_hdr-offset-equals.patch
+rm -rf target/linux/generic/backport-5.4/*
+rm -rf target/linux/generic/backport-5.4/*.*
+rm -rf target/linux/ipq807x/patches-5.4/*
+rm -rf target/linux/ipq807x/patches-5.4/*.*
+
