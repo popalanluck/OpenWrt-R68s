bf3644144fa05220342a12407e3411e88c93414d
Alan <81753503+popalanluck@users.noreply.github.com>
2023-06-05 13:51:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -36,7 +36,7 @@ rm -rf package/feeds/luci/luci-theme-argon
 # rm -rf package/feeds/kiddin9/luci-app-lorawan-basicstation
 
 # Themes
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 
 
 # 取消bootstrap为默认主题，将默认主题改为 argon
